644455e94828fdbd128b06452cd056f43102f31c
naoyashiga <naoyashiga0@gmail.com>
2015-06-08 21:57:18 +0900

    最初のコミット

---
+++ housoushitsu/ListTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ListTableViewCell.swift
+//  housoushitsu
+//
+//  Created by naoyashiga on 2015/06/08.
+//  Copyright (c) 2015年 naoyashiga. All rights reserved.
+//
+
+import UIKit
+
+class ListTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
