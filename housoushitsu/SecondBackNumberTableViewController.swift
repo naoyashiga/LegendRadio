75aff7b2c080d113e4fea719c5fbbc1a8f4b8699
naoyashiga <naoyashiga0@gmail.com>
2015-06-19 21:15:37 +0900

    バックナンバーリストを一階層下る

--- housoushitsu/SecondBackNumberTableViewController.swift
+++ housoushitsu/SecondBackNumberTableViewController.swift
@@ -1,89 +1,63 @@
 //
-//  BackNumberTableViewController.swift
+//  SecondBackNumberTableViewController.swift
 //  housoushitsu
 //
-//  Created by naoyashiga on 2015/06/16.
+//  Created by naoyashiga on 2015/06/19.
 //  Copyright (c) 2015年 naoyashiga. All rights reserved.
 //
 
 import UIKit
 
-class BackNumberTableViewController: UITableViewController {
+class SecondBackNumberTableViewController: UITableViewController {
     private let reuseIdentifier = "BackNumberTableViewCell"
     
-    private let sections = [
-        "第1~50回",
-        "第51~100回",
-        "第101~150回",
-        "第151~200回",
-        "第201~250回",
-        "第251~300回",
-        "第301~350回",
-        "第351~397回",
-        ]
-
+    var initialIndex = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let tabBarHeight = self.tabBarController?.tabBar.frame.size.height
         
-        self.tableView.contentInset = UIEdgeInsetsMake(0, 0, tabBarHeight!, 0)
-        
-
         var userDetailsNIB = UINib(nibName: reuseIdentifier, bundle: nil)
         self.tableView.registerNib(userDetailsNIB, forCellReuseIdentifier: reuseIdentifier)
         
         self.tableView.layoutMargins = UIEdgeInsetsZero
         
-        self.navigationItem.title = "バックナンバー"
+        self.navigationItem.title = "第\(initialIndex)回-第\(initialIndex + 10)回"
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return sections.count
-    }
-
-    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        return sections[section]
+        return 1
     }
     
+//    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+//        return sections[section]
+//    }
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 10
     }
-
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier, forIndexPath: indexPath) as! BackNumberTableViewCell
         
-        let start = indexPath.section * 50 + indexPath.row * 10 + 1
-        var end = 0
+        let start = initialIndex + indexPath.row
         
-        if start == 391 {
-            end = 397
-        }else{
-            end = start + 9
-        }
-
-        cell.backNumberLabel.text = "第\(start)回-第\(end)回"
+        cell.backNumberLabel.text = "第\(start)回"
         
         cell.separatorInset = UIEdgeInsetsZero
         cell.layoutMargins = UIEdgeInsetsZero
         
         return cell
     }
-
+    
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         let cell = tableView.dequeueReusableCellWithIdentifier(reuseIdentifier, forIndexPath: indexPath) as! BackNumberTableViewCell
-        let start = indexPath.section * 50 + indexPath.row * 10 + 1
+        let start = cell.backNumberLabel.text
         println(start)
-        
-        
-        let vc = SecondBackNumberTableViewController()
-        vc.initialIndex = start
-        
-        self.navigationController?.pushViewController(vc, animated: true)
     }
 }
